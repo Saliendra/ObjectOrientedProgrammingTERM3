7510e95371590e8b2a9a9018087d29fbccce3452
Ace Saliendra <111546554+Saliendra@users.noreply.github.com>
2023-05-18 10:27:04 +0800

    Create ProgrammingTeacher.java

---
+++ Inheritance/Multilevel Inheritance/ProgrammingTeacher.java
@@ -0,0 +1,12 @@
+package Inherit;
+
+class ProgrammingTeacher extends Teacher{
+		String subject = "Programming";
+		String department = "Computer Science";
+		String teachername = "Arnorld Shortman";
+		
+		void action() {
+			System.out.println("Program");
+		}
+	}
+
