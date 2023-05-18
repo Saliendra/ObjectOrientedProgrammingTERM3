547fa349bba8466c669d5262d1d44ff0e2516b3c
Ace Saliendra <111546554+Saliendra@users.noreply.github.com>
2023-05-18 10:27:27 +0800

    Create TeacherProfile.java

---
+++ Inheritance/Multilevel Inheritance/TeacherProfile.java
@@ -0,0 +1,12 @@
+package Inherit;
+
+public class TeacherProfile extends ProgrammingTeacher {
+	
+	String gender = "male";
+	int age = 26;
+	
+	void talk() {
+		System.out.println("Good morning");
+	}
+
+}
