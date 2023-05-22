a971451cf05b9d6a5415117d82c140d46b82c72a
Ace Saliendra <111546554+Saliendra@users.noreply.github.com>
2023-05-23 01:51:06 +0800

    Update AboutMe.java

--- Inheritance/LONGEXAM1/AboutMe.java
+++ Inheritance/LONGEXAM1/AboutMe.java
@@ -1,14 +1,21 @@
-package ACE;
+package WindowBuilder;
 
 import java.awt.EventQueue;
+import java.awt.Window;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.ImageIcon;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class AboutMe extends JFrame {
 
 	private JPanel contentPane;
+
 
 	/**
 	 * Launch the application.
@@ -17,8 +24,8 @@ public class AboutMe extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					AboutMe frame = new AboutMe();
-					frame.setVisible(true);
+					AboutMe frame1 = new AboutMe();
+					frame1.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -29,6 +36,7 @@ public class AboutMe extends JFrame {
 	/**
 	 * Create the frame.
 	 */
+	
 	public AboutMe() {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -39,7 +47,31 @@ public class AboutMe extends JFrame {
 		setLocationRelativeTo(null);
 
 		setContentPane(contentPane);
-	
+		contentPane.setLayout(null);
+		
+		JLabel backbtn1 = new JLabel("");
+		backbtn1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				dispose();
+				Homepage mainFr = new Homepage();
+				mainFr.setVisible(true);
+			
+				
+			}
+		});
+		backbtn1.setBounds(111, 579, 220, 49);
+		contentPane.add(backbtn1);
+		
+		JLabel meLbl = new JLabel("");
+		meLbl.setIcon(new ImageIcon("C:\\Users\\salie\\Downloads\\About.png"));
+		meLbl.setHorizontalAlignment(SwingConstants.TRAILING);
+		meLbl.setBounds(0, -39, 1366, 768);
+		contentPane.add(meLbl);
+		
+		
 	}
 
-}
+	
+	
+	}
