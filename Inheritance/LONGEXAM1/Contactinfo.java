0800636e127a73f67eec8efee7fe62cec33fe18a
Ace Saliendra <111546554+Saliendra@users.noreply.github.com>
2023-05-22 15:22:40 +0800

    Create Contactinfo.java

---
+++ Inheritance/LONGEXAM1/Contactinfo.java
@@ -0,0 +1,43 @@
+package ACE;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Contactinfo extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Contactinfo frame = new Contactinfo();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Contactinfo() {
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setBounds(100, 100, 1366, 768);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+
+		setContentPane(contentPane);
+	}
+
+}
